65476970bca6f699ab4ee8ee40882cf59b2e829e
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-10-11 07:07:18 +0200

    added clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image'
-import { generateObjectWithGpt, generateTextWithGpt } from './lib/aisdk'
-import { z } from 'zod'
-import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default async function Home() {
   return (
@@ -25,7 +23,7 @@ export default async function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <UserButton afterSwitchSessionUrl="/" />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/',
+  '/policy(.*)',
+  ])
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req) && !auth().userId) {
+    // Add custom logic to run before redirecting
+    console.log('Nieautyrozowany dostep')
+
+    return auth().redirectToSignIn()
+  }
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
